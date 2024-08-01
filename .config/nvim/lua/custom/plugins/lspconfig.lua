d69b1cfac0ee249bb22f7ac49d46ba429b875862
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-08-01 19:12:33 +0300

    Improve Lua LSP setup when editing Neovim config

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -29,9 +29,44 @@ return {
     handlers = {
       -- The default handler for all language servers.
       function(server_name)
-        require('lspconfig')[server_name].setup {
+        require('lspconfig')[server_name].setup({
+          capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        })
+      end,
+      ['lua_ls'] = function()
+        local configuration = {
           capabilities = require('cmp_nvim_lsp').default_capabilities(),
         }
+
+        if string.find(vim.fn.getcwd(), 'nvim') then
+          configuration.on_init = function(client)
+            local path = client.workspace_folders[1].name
+            ---@diagnostic disable-next-line: undefined-field
+            if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then return end
+
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              completion = { callSnippet = 'Replace' },
+              hint = { enable = true },
+              runtime = { version = 'LuaJIT' },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  -- Make the server aware of Neovim runtime files.
+                  vim.env.VIMRUNTIME,
+                  -- Make the server aware of the custom plugins.
+                  vim.fn.stdpath('data') .. '/lazy',
+                },
+              },
+            })
+          end
+          configuration.settings = {
+            Lua = {},
+          }
+        end
+
+        -- If the current neovim session is editing nvim configuration files, then
+        -- setup the Lua LSP with extra configuration.
+        require('lspconfig').lua_ls.setup(configuration)
       end,
       -- JDTLS Setup is handled by `nvim-java`.
       ['jdtls'] = function() end,
