d26d897b5e7197af39882984eabfa92cfa341d05
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-08-04 17:11:35 +0300

    Reformat init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,24 +3,25 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 -- Bootstrap lazy.nvim 💤.
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
+  vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
     '--single-branch',
     'https://github.com/folke/lazy.nvim.git',
     lazypath,
-  }
+  })
 end
 
 vim.opt.runtimepath:prepend(lazypath)
 
 -- Load custom plugins.
-require('lazy').setup {
+require('lazy').setup({
   spec = 'custom/plugins',
   change_detection = { notify = false },
   defaults = { lazy = true },
-}
+})
